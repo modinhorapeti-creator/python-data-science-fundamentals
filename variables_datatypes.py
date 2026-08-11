eb51c304ffe73b5c18a83be3aa998ca49d893a43
Modin M H <modinhorapeti@gmail.com>
2026-08-11 13:57:52 +0530

    Add variables and print data types in Python

---
+++ variables_datatypes.py
@@ -0,0 +1,15 @@
+name = "Abhi"
+age = 21
+height = 5.8 ft
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+print("\nData Types:")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
