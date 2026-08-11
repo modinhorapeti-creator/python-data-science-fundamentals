97caa69c9134b539aab62c67c9fc59f0fa12769d
Modin M H <modinhorapeti@gmail.com>
2026-08-11 14:00:02 +0530

    Add grading system based on marks

---
+++ conditional_statements.py
@@ -0,0 +1,14 @@
+marks = 75
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "F"
+
+print("Marks:", marks)
+print("Grade:", grade)
